5a4ba9b05268969acdd62839c1392170f8aed3d5
wepeqoor1 <hoopeeeee@gmail.com>
2022-06-15 21:08:37 +0300

    Add library

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import requests
 import json
 import os
 
+from custom_exception import 
 
-def get_short_link(headers: json, long_url: str) -> dict:
+
+def get_short_link(headers: json, long_url: str) -> str:
     """Convert long link to short link"""
 
     url: str = 'https://api-ssl.bitly.com/v4/shorten'
@@ -17,22 +19,22 @@ def get_short_link(headers: json, long_url: str) -> dict:
     return response.json()['link']
 
 
-def total_clicks(headers: json, bitlink: str) -> dict:
+def total_clicks(headers: json, bitlink: str) -> str:
     """Counts number of Bitlink clicks"""
 
-    bitlink: str = cut_protochol_from_url(url=bitlink)
+    bitlink: str = cut_protocol_from_url(url=bitlink)
 
     url: str = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
     response: requests.Response = requests.get(url, headers=headers)
     response.raise_for_status()
 
-    return response.json()
+    return response.json()['total_clicks']
 
 
-def is_bitlink(headers: json, bitlink: str,) -> bool:
+def is_bitlink(headers: json, bitlink: str) -> bool:
     """Check for Bitlink"""
 
-    bitlink: str = cut_protochol_from_url(url=bitlink)
+    bitlink: str = cut_protocol_from_url(url=bitlink)
 
     url: str = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}'
     response: requests.Response = requests.get(url, headers=headers)
@@ -40,12 +42,12 @@ def is_bitlink(headers: json, bitlink: str,) -> bool:
     return response.ok
 
 
-def cut_protochol_from_url(url: str) -> str:
-    """Cut protochol from URL"""
-    parsed_url: ParseResult = urlparse(url)
-    link: str = ''.join([parsed_url.hostname, parsed_url.path])
+def cut_protocol_from_url(url: str) -> str:
+    """Cut protocol from URL"""
 
-    return link
+    parsed_url: ParseResult = urlparse(url)
+    
+    return ''.join([parsed_url.hostname, parsed_url.path])
 
 
 if __name__=='__main__':
@@ -54,20 +56,16 @@ if __name__=='__main__':
         "Authorization": f"Bearer {os.environ.get('API_TOKEN')}"
     }
 
-    link: str = str(input('Введите ссылку или Битлинк: '))
+    link: str = input('Введите ссылку или Битлинк: ')
 
     try:
         is_bitlink: bool = is_bitlink(bitlink=link, headers=headers)
-        print('is_bitlink', is_bitlink)
         response = (
             total_clicks(headers=headers, bitlink=link) 
             if is_bitlink 
             else get_short_link(headers=headers, long_url=link)
         )
-    except:
+    except requests.exceptions.HTTPError:
         print('Не корректный запрос')
     else:
         print(response)
-
-
-
